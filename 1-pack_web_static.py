606d7e7bcca1db992016e960e3600705975e9635
Kouam Franck <kouamfranck96@gmail.com>
2022-10-08 11:26:49 +0100

    Correcting errors

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 """
-Fabric script to genereate tgz archive
+Fabric script to generate a .tgz archive
 execute: fab -f 1-pack_web_static.py do_pack
 """
 
-from datetime import datetime
-from fabric.api import *
+from fabric.api import local
+import datetime as dt
+import os
 
 
 def do_pack():
     """
-    making an archive on web_static folder
+    Making an archive on web_static folder
     """
 
-    time = datetime.now()
-    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
-    local('mkdir -p versions')
-    create = local('tar -cvzf versions/{} web_static'.format(archive))
-    if create is not None:
-        return archive
-    else:
-        return None
+    local("mkdir -p versions")
+    d = dt.datetime.now()
+    name = f"versions/web_static_{d.year}{d.month}{d.day}"\
+           + f"{d.hour}{d.minute}{d.second}.tgz"
+    local("tar cvfz {} web_static".format(name))
+    if os.path.exists(name):
+        return os.path.abspath(name)
