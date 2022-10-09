c3c4983d4a6eb40d157842b11525d8e1df97c893
Kouam Franck <kouamfranck96@gmail.com>
2022-10-10 00:02:04 +0100

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """
 Fabric script to deploy tgz archive
-execute: fab -f 2-do_deploy_web_static.py do_deploy
+execute: fab -f 2-do_deploy_web_static.py do_deploy:archive_path=filepath
+    -i private-key -u user
 """
 
 from fabric.api import *
 from os.path import exists
+
+
 env.hosts = ['34.239.160.19', '34.231.243.196']
 
 
@@ -15,17 +18,21 @@ def do_deploy(archive_path):
     """
     if not exists(archive_path):
         return False
-    archive_fname = archive_path.split('/')[-1]
-    archive_bname = archive_fname.split('.')[0]
-    put(archive_path, '/tmp/')
-    run('mkdir -p /data/web_static/releases/{}'.format(archive_bname))
-    run('tar -xzf /tmp/{0}.tgz -C /data/web_static/releases/{0}'.format(
-                archive_bname))
-    run(('mv /data/web_static/releases/{0}/web_static/* ' +
-        '/data/web_static/releases/{0}/').format(archive_bname))
-    run('rm -rf /data/web_static/releases/{}/web_static'.format(archive_bname))
-    run('rm -f /tmp/{}'.format(archive_fname))
-    run('rm -f /data/web_static/current')
-    run('ln -s /data/web_static/releases/{}'.format(archive_bname)
-        + ' /data/web_static/current')
-    return True
+    try:
+        archive_fname = archive_path.split('/')[-1]
+        archive_bname = archive_fname.split('.')[0]
+        put(archive_path, '/tmp/')
+        run('mkdir -p /data/web_static/releases/{}'.format(archive_bname))
+        run('tar -xzf /tmp/{0}.tgz -C /data/web_static/releases/{0}'.format(
+                    archive_bname))
+        run(('mv /data/web_static/releases/{0}/web_static/* ' +
+            '/data/web_static/releases/{0}/').format(archive_bname))
+        run('rm -rf /data/web_static/releases/{}/web_static'.format(
+            archive_bname))
+        run('rm -f /tmp/{}'.format(archive_fname))
+        run('rm -f /data/web_static/current')
+        run('ln -s /data/web_static/releases/{}'.format(archive_bname)
+            + ' /data/web_static/current')
+        return True
+    except Exception:
+        return False
