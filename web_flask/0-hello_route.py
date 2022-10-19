3b013dcb08928234c094856758d4f5c018140438
Kouam Franck <kouamfranck96@gmail.com>
2022-10-20 00:35:33 +0100

    Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """ Prints a Message when / is called """
-    return 'Hello HBNB!\n'
+    return 'Hello HBNB!'
 
 
 if __name__ == "__main__":
