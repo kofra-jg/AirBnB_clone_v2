08a64526a6cd22462bc248fb51519c6b9af24069
Kouam Franck <kouamfranck96@gmail.com>
2022-10-20 01:30:24 +0100

    Number in a template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" A script that starts a flask web application """
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Prints a Message when / is called """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Prints a Message when /HBNB is called """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def ciscfun(text):
+    """ Prints a Message when /c/<text> is called """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """ Prints a Message when /c/<text> is called """
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n: int):
+    """ Prints a number"""
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def numbertemplate(n: int):
+    """ Prints a number in a template """
+    return render_template('5-number.html', number=n)
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
