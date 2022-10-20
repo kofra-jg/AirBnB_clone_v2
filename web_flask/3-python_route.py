660ad3b13915f6f952ecc6cefdc0ddf425c53de4
Kouam Franck <kouamfranck96@gmail.com>
2022-10-20 01:11:13 +0100

    Python routes

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" A script that starts a flask web application """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Prints a Message when / is called """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Prints a Message when /HBNB is called """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def ciscfun(text):
+    """ Prints a Message when /c/<text> is called """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """ Prints a Message when /c/<text> is called """
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
